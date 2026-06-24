710bea578e1cb9d57ef3c782f32c71307de3eb2f
Victor Estevam <victorestevamp@>
2026-06-24 13:52:31 -0300

    feat: commit dos arquivos de configuração iniciais

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+    <section className="bg-bg-main text-text-light-gray">
+      <h1>saaaaaaaaaaaaa</h1>
+      <p>fffffffffff</p>
+    </section>
+    </>
+  )
+}
+
+export default App
